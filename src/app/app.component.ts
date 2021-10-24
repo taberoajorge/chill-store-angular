8bd210815f626d14e79301c510f30dd6ff7a3c5d
Jorge Alexander Taberoa Jimenez <65203661+taberoajorge@users.noreply.github.com>
2021-10-23 22:28:28 -0500

    Estructuras condicionales y de ciclos
    Practica de uso de estructura condicionales

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,55 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.sass']
+  styleUrls: ['./app.component.sass'],
+  // template : `
+
+  // <ng-template [ngTemplateOutlet]="outlet" [ngOutletContext]="context"></ng-template>
+
+
+  // `
+
 })
 export class AppComponent {
-  title = 'my-store';
+  title : string = 'my-store';
+  buttonStatus : boolean = true;
+  newName: any = '';
+
+  person = {
+    name: 'Jorge',
+    age: 21,
+    avantar: 'https://www.gravatar.com/avatar/205e460b479e2e5b48aec07710c08d50'
+  }
+
+  emojis : any[] = [ '😂' , '🐦', '🐳','🌮', '💚'];
+
+  addElement() {
+    this.emojis.push(this.newName);
+    this.newName = '';
+  }
+
+  removeEmoji(index : number) {
+    this.emojis.splice(index, 1);
+  }
+
+
+  toggleButton() {
+    this.buttonStatus = !this.buttonStatus;
+  }
+
+  ageIncrease() {
+    this.person.age++;
+  }
+
+  onScroll(event : Event) {
+   const element = event.target as HTMLElement;
+   console.log(element.scrollTop);
+  }
+
+  onKeyUp(event : KeyboardEvent) {
+    const element = event.target as HTMLInputElement;
+    this.person.name = element.value;
+  }
+
 }
+
