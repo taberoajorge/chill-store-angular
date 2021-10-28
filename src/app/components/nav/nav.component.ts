aeb8aea7e62a131b391ea615d1531f584e823aa1
Jorge Alexander Taberoa Jimenez <65203661+taberoajorge@users.noreply.github.com>
2021-10-28 14:06:29 -0500

    Add html and css to nav

--- src/app/components/nav/nav.component.ts
+++ src/app/components/nav/nav.component.ts
@@ -3,13 +3,18 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-nav',
   templateUrl: './nav.component.html',
-  styleUrls: ['./nav.component.sass']
+  styleUrls: ['./nav.component.scss']
 })
 export class NavComponent implements OnInit {
+
+  changeMenu: boolean =false;
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+  toogleMenu(){
+    this.changeMenu = !this.changeMenu;
   }
 
 }
